f196a3dd5a9892e7d2e34543085cdae742104832
Gergely Kiss <gergely@equipe.agency>
2016-10-12 14:11:43 +0200

    Gulp tasks

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // Setup basic express server
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-// var server_port = process.env.ADFURY_PORT_8080_TCP_PORT || 8080;
-// var server_ip_address = process.env.ADFURY_PORT_8080_TCP_ADDR || '127.0.0.1';
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-//var port = process.env.PORT || 3000;
+
+var server = require('http').createServer(app).listen(server_port ,server_ip_address, function () {
+    console.log("✔ Express server listening at %s:%d ", server_ip_address,server_port);
+});;
+
+var io = require('socket.io')(server);
 
 console.log(process.env.OPENSHIFT_NODEJS_IP, process.env.OPENSHIFT_NODEJS_PORT);
 
