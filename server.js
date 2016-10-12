140a208393b2aacf6b5c9e0eb5f50cedea4c668d
Gergely Kiss <gergely@equipe.agency>
2016-10-12 14:17:40 +0200

    Environment

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ var app = express();
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-var server = require('http').createServer(app).listen(server_port ,server_ip_address, function () {
-    console.log("✔ Express server listening at %s:%d ", server_ip_address,server_port);
-});;
-
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 console.log(process.env.OPENSHIFT_NODEJS_IP, process.env.OPENSHIFT_NODEJS_PORT);
