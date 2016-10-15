f1bf850d005345064ac0626d4899d5c7617de059
Kashterion <advertonic.kft@gmail.com>
2016-10-15 18:18:36 +0200

    Environment test

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // Setup basic express server
 var express = require('express');
 var app = express();
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.ADFURY_SERVICE_PORT || 8080;
+var server_ip_address = process.env.ADFURY_SERVICE_HOST || '127.0.0.1';
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-console.log(process.env.OPENSHIFT_NODEJS_IP, process.env.OPENSHIFT_NODEJS_PORT);
+console.log(process.env.ADFURY_SERVICE_HOST, process.env.ADFURY_SERVICE_PORT);
 
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
@@ -32,7 +32,7 @@ io.on('connection', function (socket) {
     });
   });
 
-  // when the client emits 'add user', this listens and executes
+  // when the client emits 'add user', this listens and executees
   socket.on('add user', function (username) {
     if (addedUser) return;
 
